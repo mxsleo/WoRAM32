fcf0671f216f07fb6f284f3d35cce5c0520c87d3
Maxim Leo <68343138+mxsleo@users.noreply.github.com>
2024-03-19 21:05:29 +0300

    Open sources

---
+++ woram32.cpp
@@ -0,0 +1,232 @@
+#include "woram32.h"
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+
+    WNDCLASS Woram32MainClass = {0};
+    Woram32MainClass.lpfnWndProc = Woram32MainProcedure;
+    Woram32MainClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON));
+    Woram32MainClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    Woram32MainClass.lpszClassName = W32_CLASS;
+
+    if (!RegisterClass(&Woram32MainClass)) { return -1; }
+
+    HWND hWnd = CreateWindow(W32_CLASS, W32_TITLE, WS_POPUP | WS_VISIBLE, W32_POSX, W32_POSY, W32_SZX, W32_SZY, NULL, NULL, NULL, NULL);
+
+    SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+    SetLayeredWindowAttributes(hWnd, 0, W32_OPACITY, LWA_ALPHA);
+
+    SetWindowLong(hWnd, GWL_HWNDPARENT, (LONG)::GetDesktopWindow());
+    ShowWindow(hWnd, SW_HIDE);
+    ShowWindow(hWnd, SW_SHOW);
+
+    MSG Woram32MainMessage = {0};
+    while (GetMessage(&Woram32MainMessage, NULL, 0, 0)) {
+        TranslateMessage(&Woram32MainMessage);
+        DispatchMessage(&Woram32MainMessage);
+    }
+
+    woram32ThreadContinue = false;
+    TerminateThread(woram32Thread, 0);
+
+    return 0;
+
+}
+
+void WoramExit() {
+
+    woram32ThreadContinue = false;
+    TerminateThread(woram32Thread, 0);
+    PostQuitMessage(0);
+
+}
+
+LRESULT CALLBACK Woram32MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+
+    switch (msg) {
+
+        case WM_CREATE:
+            Woram32MainAddWidgets(hWnd);
+            woram32Thread = CreateThread(NULL, 0, Woram32MainSpook, NULL, 0, NULL);
+            break;
+
+        case WM_DESTROY: case WM_NCRBUTTONUP:
+            WoramExit();
+            break;
+
+        case WM_NCHITTEST: {
+            LRESULT hit = DefWindowProc(hWnd, msg, wp, lp);
+            if (hit == HTCLIENT) hit = HTCAPTION;
+            return hit;
+        }
+
+        case WM_MOUSEWHEEL:
+            if (GET_WHEEL_DELTA_WPARAM(wp) > 0 and woram32MainOpacity < W32_OPA_MAX)
+                woram32MainOpacity += W32_OPA_STEP;
+            else if (woram32MainOpacity > W32_OPA_MIN)
+                woram32MainOpacity -= W32_OPA_STEP;
+            SetLayeredWindowAttributes(hWnd, 0, woram32MainOpacity, LWA_ALPHA);
+            break;
+
+        case WM_NCMBUTTONUP:
+            setNumber = (setNumber + 1) % SET_AMT;
+            break;
+
+        case WM_KEYUP:
+
+            switch (wp) {
+
+                case VK_ESCAPE:
+                    WoramExit();
+                    break;
+
+                case VK_UP: case VK_RIGHT:
+                    setNumber = (setNumber + 1) % SET_AMT;
+                    break;
+
+                case VK_DOWN: case VK_LEFT:
+                    setNumber = (setNumber - 1 + SET_AMT) % SET_AMT;
+                    break;
+
+                case VK_NUMPAD1: case 0x31:
+                    setNumber = 0 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD2: case 0x32:
+                    setNumber = 1 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD3: case 0x33:
+                    setNumber = 2 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD4: case 0x34:
+                    setNumber = 3 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD5: case 0x35:
+                    setNumber = 4 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD6: case 0x36:
+                    setNumber = 5 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD7: case 0x37:
+                    setNumber = 6 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD8: case 0x38:
+                    setNumber = 7 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD9: case 0x39:
+                    setNumber = 8 % SET_AMT;
+                    break;
+
+                case VK_NUMPAD0: case 0x30:
+                    setNumber = SET_AMT - 1;
+                    break;
+
+            }
+
+            break;
+
+    }
+
+    return DefWindowProc(hWnd, msg, wp, lp);
+
+}
+
+void Woram32MainAddWidgets(HWND hWnd) {
+
+    HFONT hFontNum = CreateFont(-W32_TEXT_SMALL, 0, 0, 0, FW_DONTCARE, FALSE, TRUE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, NULL);
+    HFONT hFontOut = CreateFont(-W32_TEXT_LARGE, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, NULL);
+
+    hNum = CreateWindow("static", "99", WS_VISIBLE | WS_CHILD | ES_RIGHT, 0, 0, W32_SZY, W32_SZY, hWnd, NULL, NULL, NULL);
+    hOut = CreateWindow("static", "-99.99 -99.99 -99.99 -99.99", WS_VISIBLE | WS_CHILD | ES_CENTER, W32_SZY, 0, W32_SZX - W32_SZY, W32_SZY, hWnd, NULL, NULL, NULL);
+
+    SendMessage(hNum, WM_SETFONT, (WPARAM)hFontNum, TRUE);
+    SendMessage(hOut, WM_SETFONT, (WPARAM)hFontOut, TRUE);
+
+}
+
+void Woram32ThreadedSleep() {
+
+    sprintf_s(outNum, "%d", setNumber + 1);
+    SetWindowText(hNum, outNum);
+
+    Sleep(WSDELAY);
+
+}
+
+DWORD WINAPI Woram32MainSpook(LPVOID lpParameter) {
+
+    HANDLE hProcess;
+
+    while (!(hProcess = OpenProcess(PROCESS_VM_READ, false, Woram32MainSpookGetProcessID()))) {
+        Woram32ThreadedSleep();
+    }
+
+    LPCVOID variablePtr = nullptr;
+    LPVOID variableBuffer = nullptr;
+    double values[VAL_AMT];
+    CHAR out[W32_CSLONGLEN];
+
+    while (woram32ThreadContinue) {
+
+        for (size_t i = 0; i < VAL_AMT; ++i) {
+
+            variablePtr = reinterpret_cast<LPCVOID>(VALUE_POINTERS[setNumber][i]);
+            variableBuffer = reinterpret_cast<LPVOID>(&values[i]);
+
+            while (!ReadProcessMemory(hProcess, variablePtr, variableBuffer, sizeof(double), nullptr)) {
+                while (!(hProcess = OpenProcess(PROCESS_VM_READ, false, Woram32MainSpookGetProcessID()))) {
+                    Woram32ThreadedSleep();
+                }
+            }
+
+        }
+
+        sprintf_s(out, "%.2f", values[0]);
+        for (size_t i = 1; i < VAL_AMT; ++i) {
+            sprintf_s(out, "%s %.2f", out, values[i]);
+        }
+
+        SetWindowText(hOut, out);
+
+        Woram32ThreadedSleep();
+
+    }
+
+    CloseHandle(hProcess);
+
+    return 0;
+
+}
+
+DWORD Woram32MainSpookGetProcessID() {
+
+    DWORD dwProcessID = 0;
+
+    HANDLE hSnapshot;
+    while ((hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)) == INVALID_HANDLE_VALUE) {
+        Sleep(WSDELAY);
+    }
+
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    BOOL hResult = Process32First(hSnapshot, &pe);
+    while (hResult) {
+        if (!strcmp(PROCESS, pe.szExeFile) || !strcmp(PROCALT, pe.szExeFile)) {
+            dwProcessID = pe.th32ProcessID;
+            break;
+        }
+        hResult = Process32Next(hSnapshot, &pe);
+    }
+
+    CloseHandle(hSnapshot);
+
+    return dwProcessID;
+
+}
---
+++ woram32.h
@@ -0,0 +1,65 @@
+#ifndef WORAM32_H_INCLUDED
+#define WORAM32_H_INCLUDED
+
+#include <cstdio>
+#include <windows.h>
+#include <tlhelp32.h>
+
+#define IDI_ICON        1
+
+#define W32_CLASS       "Woram32MainClass"
+#define W32_TITLE       "iexplore"
+#define W32_SZY         16
+#define W32_SZX         160 + W32_SZY
+#define W32_POSX        (GetSystemMetrics(SM_CXSCREEN) - W32_SZX) / 2
+#define W32_POSY        (GetSystemMetrics(SM_CYSCREEN) - W32_SZY) / 2
+#define W32_OPACITY     32
+#define W32_OPA_STEP    8
+#define W32_OPA_MIN     W32_OPA_STEP
+#define W32_OPA_MAX     256 - W32_OPA_STEP
+#define W32_TEXT_SMALL  10
+#define W32_TEXT_LARGE  12
+#define W32_CSSHORTLEN  8
+#define W32_CSLONGLEN   256
+
+UINT32 woram32MainOpacity = W32_OPACITY;
+
+HWND hNum;
+HWND hOut;
+
+LRESULT CALLBACK Woram32MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+void Woram32MainAddWidgets(HWND hWnd);
+
+#define WSDELAY 250
+
+bool woram32ThreadContinue = true;
+HANDLE woram32Thread;
+void Woram32ThreadedSleep();
+DWORD WINAPI Woram32MainSpook(LPVOID lpParameter);
+
+#define PROCESS "Project1.exe"
+#define PROCALT "ProjectModul4.exe"
+
+#define VAL_AMT 4
+#define SET_AMT 12
+UINT32 const VALUE_POINTERS[SET_AMT][VAL_AMT] = {
+    {0x479C14, 0x479C1C, 0x479C24, 0x479C2C},
+    {0x47AC14, 0x47AC1C, 0x47AC24, 0x47AC2C},
+    {0x47BC24, 0x47BC2C, 0x47BC34, 0x47BC3C},
+    {0x47BC24, 0x47BC2C, 0x47BC34, 0x47BC3C},
+    {0x476C0C, 0x476C14, 0x476C1C, 0x476C24},
+    {0x476C14, 0x476C1C, 0x476C24, 0x476C2C},
+    {0x477C14, 0x477C1C, 0x477C24, 0x477C2C},
+    {0x476C14, 0x476C1C, 0x476C24, 0x476C2C},
+    {0x477C14, 0x477C1C, 0x477C24, 0x477C2C},
+    {0x476C14, 0x476C1C, 0x476C24, 0x476C2C},
+    {0x477C14, 0x477C1C, 0x477C24, 0x477C2C},
+    {0x477C14, 0x477C1C, 0x477C24, 0x477C2C}
+};
+
+size_t setNumber = 0;
+CHAR outNum[W32_CSSHORTLEN];
+
+DWORD Woram32MainSpookGetProcessID();
+
+#endif // WORAM32_H_INCLUDED
